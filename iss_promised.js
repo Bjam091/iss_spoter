df85b5486ca2b1d0b90dfcaba4616ad7f29fb571
Brandon <brandonjamiller@hotmail.com>
2019-09-27 16:36:04 +0000

    iss_promised.js completed, and eslint

---
+++ iss_promised.js
@@ -0,0 +1,29 @@
+const request = require('request-promise-native');
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTime)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://ipvigilante.com/json/${ip}`);
+};
+
+const fetchISSFlyOverTime = function(body) {
+  const { latitude, longitude } = JSON.parse(body).data;
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTime, nextISSTimesForMyLocation };
